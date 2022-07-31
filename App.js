d4ed0e5092768316e6ea5200eab1f38e50b0bfb3
arsiihere <arsiiahmi@gmail.com>
2022-08-01 00:00:30 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Shuru Allah Kay Naam se jo bara mehrban aur nehayat reham karnay wala
+        hai. ALLAH MUHAMMAD
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
